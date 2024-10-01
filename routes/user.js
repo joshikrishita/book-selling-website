f618c1bee52b7e0fda6b2646cd7e9a3f03b45db8
joshikrishita <joshikrishita@outlook.com>
2024-10-01 18:07:51 +0530

    book-selling

---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const app=express();
+
+app.push("/user/signup",function(res,req){
+    
+})
+
+app.push("/user/signin",function(res,req){
+    
+})
+//sees all the purchased books
+app.get("/user/purchased",function(res,req){
+    
+})
+
+app.listen(3001);
